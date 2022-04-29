8b873de0e5fe8733fa869799aa0c9a0060a9afba
Angela Sanchez <a-sanchez@uadec.edu.mx>
2022-04-29 14:16:27 -0500

    icon

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>SantoVittoria</title>
+    <link rel="icon" type="image/png" href='{{asset("images/favicon-32x32.png")}}'/>
+    <title>SantoVittoria
+    </title>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
